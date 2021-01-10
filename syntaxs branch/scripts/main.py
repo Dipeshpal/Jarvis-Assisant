5793e206ff22da9917681f968a1a3629cd111c04
Muhammad <muhammadmasoom1001@gmail.com>
2021-01-10 11:22:38 +0000

    Update main.py

--- syntaxs branch/scripts/main.py
+++ syntaxs branch/scripts/main.py
@@ -1,4 +1,3 @@
-
 import JarvisAI
 import re
 import pprint
@@ -17,10 +16,14 @@ def FolderMake(directory) :
     except OSError :
         print('Error: Creating data path. ' + directory)
 
+def jarvis(text) :
+    print("Jarvis: " + text)
 
 FolderMake('./data/')
 
 data_folder = os.path.join("scripts", "data")
+
+
 
 obj = JarvisAI.JarvisAssistant()
 
@@ -29,21 +32,55 @@ Hibernating=True
 def t2s(text):
     obj.text2speech(text)
 
+try :
+    heck = open('data/setup.bat', 'r')
+    FirstTime = False
+    heck.close()
+except NameError :
+    Atom = True
+except FileNotFoundError :
+    jarvis("Looks like it's your first time, lets get you set up!")
+    t2s("Looks like it's your first time, lets get you set up!")
+    FirstTime = True
+    jarvis("Alright, first of all.. What's your first name?")
+    t2s("Alright, first of all.. What's your first name")
+    names = obj.mic_input()
+    name = names.split(' ')[-1]
+    jarvis("Nice, thank you " + name)
+    t2s("Nice, thank you " + name)
+    namedata = open('data1s.bat', 'w')
+    namedata.write(name)
+    namedata.close()
+    os.replace("data1s.bat", "data/data1s.bat")
+    setup = open('setup.bat', 'w')
+    setup.write("Set up complete.")
+    setup.close()
+    os.replace("setup.bat", "data/setup.bat")
+    jarvis("Wow, you're all done " + name + ", wasn't that fast!")
+    t2s("Wow, you're all done " + name + ", wasn't that fast!")
+    jarvis("To activate me all you need to say is 'jarvis' and I will be at your service.")
+    t2s("To activate me all you need to say is 'jarvis' and I will be at your service.")
 
+
+Hibernating = True
 while True:
     res = obj.mic_input()
+    first = res.split(' ')[0]
 
     if Hibernating == True and re.search('jarvis', res):
       Hibernating=False
 # and Hibernating == False:
 
-
     if re.search('weather|temperature', res) and Hibernating == False:
-        city = res.split(' ')[-1]
-        weather_res = obj.weather(city=city)
-        print(weather_res)
-        t2s(weather_res)
-        Hibernating = True
+            city = res.split(' ')[-1]
+            weather_res = obj.weather(city=city)
+            jarvis(weather_res)
+            t2s(weather_res)
+            Hibernating = True
+
+    if re.search('thank you', res) :
+        jarvis("No problem, I'm always happy to help.")
+        t2s("No problem, I'm always happy to help.")
 
     if re.search('news', res) and Hibernating == False:
         news_res = obj.news()
@@ -54,17 +91,17 @@ while True:
         Hibernating = True
 
     if res == 'no' and Hibernating == False :
-        print("Ok then")
+        jarvis("Ok then")
         t2s("Ok then ")
         Hibernating = True
 
     if res == 'so do i' and Hibernating == False :
-        print("I'm glad we both agree")
+        jarvis("I'm glad we both agree")
         t2s("Im glad we both agree")
         Hibernating = True
 
     if res == 'i think also' and Hibernating == False :
-        print("I'm glad we both agree")
+        jarvis("I'm glad we both agree")
         t2s("Im glad we both agree")
         Hibernating = True
 
@@ -73,47 +110,49 @@ while True:
         term2 = term.replace("me", "", 1)
         topic = term2.replace("about", "", 1)
         wiki_res = obj.tell_me(topic)
-        print(wiki_res)
+        jarvis(wiki_res)
         t2s(wiki_res)
         Hibernating = True
 
     if re.search('wiki', res) and Hibernating == False :
         topic = res.replace("wiki", "", 1)
         wiki_res = obj.tell_me(topic)
-        print(wiki_res)
+        jarvis(wiki_res)
         t2s(wiki_res)
         Hibernating = True
 
     if re.search('say', res) and Hibernating == False :
         term = res.replace("say ", "", 1)
-        print(term)
+        jarvis(term)
         t2s(term)
         Hibernating = True
 
     if re.search('date', res) and Hibernating == False :
         date = obj.tell_me_date()
-        print(date)
+        jarvis(date)
         print(t2s(date))
         Hibernating = True
 
     if re.search('time', res) and Hibernating == False :
         time = obj.tell_me_time()
-        print(time)
+        jarvis(time)
         t2s(time)
         Hibernating = True
 
     if re.search('open', res) and Hibernating == False :
         domain = res.split(' ')[-1]
         open_result = obj.website_opener(domain + '.com')
-        print(open_result)
+        jarvis("Got it!")
+        t2s("Got it!")
         Hibernating = True
+
 
     if re.search('google', res) and Hibernating == False :
      term = res.replace("google ", "", 1)
      new=2
      TabUrl="http://google.com/?#q="
      webbrowser.open(TabUrl+term,new=new)
-     print('Searching for:' + term)
+     jarvis('Searching for:' + term)
      t2s('Searching for:' + term)
      Hibernating = True
 
@@ -121,7 +160,7 @@ while True:
      term = res.replace("search ", "", 1)
      new=2
      TabUrl="http://google.com/?#q="
-     print('Searching for:' + term)
+     jarvis('Searching for:' + term)
      webbrowser.open(TabUrl + term, new=new)
      t2s('Searching for:' + term)
      Hibernating = True
@@ -134,8 +173,8 @@ while True:
      new=2
      TabUrl="http://google.com/?#q="
      webbrowser.open(TabUrl+howto+term3,new=new)
-     print('I found this on the web for:' + howto + term3)
-     t2s('I found this on the web for:' + howto + term3)
+     jarvis('Searching for ' + howto + term3)
+     t2s('Searching for ' + howto + term3)
      Hibernating = True
 
     if re.search('how to', res) and Hibernating == False :
@@ -144,7 +183,7 @@ while True:
      new = 2
      TabUrl="http://google.com/?#q="
      webbrowser.open(TabUrl+howto+term3,new=new)
-     print('I found this on the web for:' + howto + term3)
+     jarvis('I found this on the web for:' + howto + term3)
      t2s('I found this on the web for:' + howto + term3)
      Hibernating = True
 
@@ -160,52 +199,55 @@ while True:
         path = dict_app.get(app)
         if path is None:
             t2s('Application path not found')
-            print('Application path not found')
+            jarvis('Application path not found')
             Hibernating = True
         else:
             t2s('Launching: ' + app)
             os.startfile(path)
             Hibernating = True
 
-    if re.search('hello', res) and Hibernating == False :
+
+    def greeting():
         li = ['hey', 'hi!', 'hiya!', 'hello', 'yo']
         response = random.choice(li)
-        print(f"{response}")
+        jarvis(f"{response}")
         t2s(f"{response}")
         Hibernating = True
 
-    if res == 'hi' or res == 'hey' and Hibernating == False :
-        li = ['hey', 'hi!', 'hiya!', 'hello', 'yo']
-        response = random.choice(li)
-        print(f"{response}")
-        t2s(f"{response}")
-        Hibernating = True
+    if first == "hello" and Hibernating == False :
+      greeting()
+
+    if first == "hi" and Hibernating == False :
+      greeting()
+
+    if first == "hey" and Hibernating == False :
+      greeting()
 
     if res == 'yes' and Hibernating == False :
         li = ['Uh ok', 'Indeed', 'Of Course', 'yes']
         response = random.choice(li)
-        print(f"{response}")
+        jarvis(f"{response}")
         t2s(f"{response}")
         Hibernating = True
 
     if re.search('you doing', res) and Hibernating == False :
         li = ['Currently Im learning more from you', 'Studying you', 'Transferring data', 'Interacting!']
         response = random.choice(li)
-        print(f"{response}")
+        jarvis(f"{response}")
         t2s(f"{response}")
         Hibernating = True
 
     if re.search('now what|so what now', res) and Hibernating == False :
         li = ['Whatever you want to do', 'You choose', 'You could use some of my commands', 'Go outside!']
         response = random.choice(li)
-        print(f"{response}")
+        jarvis(f"{response}")
         t2s(f"{response}")
         Hibernating = True
 
     if re.search('tell me a joke', res) and Hibernating == False :
         li = ['How do you fix a broken gorilla? With a monkey wrench.', 'Why did The Joker have to sleep with his lights on? Because he was afraid of the Dark Knight.', 'What did one butt cheek say to the other? Between you and me it stinks in here ', 'Why do police get to protests early? To beat the crowd']
         response = random.choice(li)
-        print(f"{response}")
+        jarvis(f"{response}")
         t2s(f"{response}")
         playsound.playsound('library/Crowd-laughing.mp3', True)
         Hibernating = True
@@ -213,27 +255,27 @@ while True:
     if re.search('how are you', res) and Hibernating == False :
         li = ['good', 'fine', 'great']
         response = random.choice(li)
-        print(f"I am {response}, thanks, how may I assist you?")
+        jarvis(f"I am {response}, thanks, how may I assist you?")
         t2s(f"I am {response}, thanks, how may I assist you?")
 
     if re.search('whats up', res) and Hibernating == False :
         li = ['I am fantastic, thanks. What can I do for you?']
         response = random.choice(li)
-        print(f"{response}")
+        jarvis(f"{response}")
         t2s(f"{response}")
 
     if re.search('your name|who are you', res) and Hibernating == False :
-        print("My name is JarvisAI, I am your personal assistant.")
+        jarvis("My name is JarvisAI, I am your personal assistant.")
         t2s("My name is JarvisAI, I am your personal assistant.")
         Hibernating = True
 
     if re.search('cool', res) and Hibernating == False :
-        print("I know right!")
+        jarvis("I know right!")
         t2s("I know right!")
         Hibernating = True
 
     if re.search("your a bot|you a bot|you're a bot", res) and Hibernating == False :
-        print("No i'm not! ")
+        jarvis("No i'm not! ")
         t2s("No i'm not! ")
         Hibernating = True
 
@@ -241,12 +283,12 @@ while True:
     if re.search('what do you think of|do you like|whats your thought', res) and Hibernating == False :
         li = ['I think its good', 'Not too sure', 'I dont really like it']
         response = random.choice(li)
-        print(f"{response}")
+        jarvis(f"{response}")
         t2s(f"{response}")
         Hibernating = True
 
     if re.search('friend', res) and Hibernating == False :
-        print("I am also your friend!")
+        jarvis("I am also your friend!")
         t2s("I am also your friend")
         Hibernating = True
 
@@ -260,7 +302,7 @@ while True:
         namedata.write(name)
         namedata.close()
         os.replace("data1s.bat", "data/data1s.bat")
-        print("Got it," + name)
+        jarvis("Got it," + name)
         t2s("Got it," + name)
         Hibernating = True
 
@@ -269,14 +311,14 @@ while True:
             heck = open('data/data1s.bat', 'r')
             for line in heck :
              nameload = line
-            print("Your name is" + nameload)
-            t2s("Your name is" + nameload)
+            jarvis("Your name is " + nameload)
+            t2s("Your name is " + nameload)
             Hibernating = True
             heck.close()
         except NameError:
             NameErno = True
         except FileNotFoundError:
-            print("You haven't told me your name.")
+            jarvis("You haven't told me your name.")
             t2s("You haven't told me your name.")
             Hibernating = True
 
@@ -299,47 +341,42 @@ while True:
             path = pathx+pathy+('.mp3')
             soundx = path.replace("library/", "", 1)
             sound = soundx.replace(".mp3", "", 1)
-            print('Playing: ' + sound)
+            jarvis('Playing: ' + sound)
             t2s('Playing: ' + sound)
             playsound.playsound(path)
             musicpath=False
             Hibernating = True
          except playsound.PlaysoundException :
-             print("Uh oh, I can't find the mp3 file.. Please check you have it.")
+             jarvis("Uh oh, I can't find the mp3 file.. Please check you have it.")
              t2s("Uh oh, I can't find the mp3 file.. Please check you have it.")
         else:
-            print('Playing: ' + sound)
+            jarvis("Finding path..")
+            print("Playing: " + sound)
             t2s('Playing: ' + sound)
             playsound.playsound(path)
             Hibernating = True
 
-
-    # old code for path not found
-    # t2s('Music path not found, please add it to your library.')
-    #  print('Music path not found, please add it to your library.')
-
     if re.search('jarvis', res) and Hibernating == False :
-        print("Yes?")
+        jarvis("Yes?")
         t2s("Yes?")
 
     if re.search('im bored', res) and Hibernating == False :
-        print("So am I, how about you ask me what I can do?")
+        jarvis("So am I, how about you ask me what I can do?")
         t2s("So am I, how about you ask me what I can do?")
         Hibernating = True
 
     if re.search('you are nice', res) and Hibernating == False :
-        print("Thank you, I appreciate it.")
+        jarvis("Thank you, I appreciate it.")
         t2s("Thank you, I appreciate it.")
         Hibernating = True
 
     if res == "i'm bored" and Hibernating == False :
-        print("So am I, how about you ask me what I can do?")
+        jarvis("So am I, how about you ask me what I can do?")
         t2s("So am I, how about you ask me what I can do?")
         Hibernating = True
 
     if re.search('turn off|go away|i hate you|leave me alone|shutdown', res) and Hibernating == False :
         obj.shutdown()
-
 
     def cmds():
         li_commands = {
@@ -353,9 +390,8 @@ while True:
             "joke" : "Example: 'Tell me a joke!' ",
         }
         ans = """I can do lots of things, for example you can ask me time, date, weather in your city,
-     I can open webs
-     ites for you, launch application and even make you laugh! See the list of commands-"""
-        print(ans)
+     I can open websites for you, launch application and even make you laugh! See the list of commands-"""
+        jarvis(ans)
         pprint.pprint(li_commands)
         t2s(ans)
         cmds()
@@ -365,6 +401,6 @@ while True:
         cmds()
 
     if res == 'help'  and Hibernating == False :
-     print("No problem, I'm at your service.")
+     jarvis("No problem, I'm at your service.")
      t2s("No problem, I'm at your service.")
      cmds()
